9198301d03d6c614a7386a3a48d48f726c85eabe
alvaro <delandaluce_alvaro@wheatoncollege.edu>
2017-05-18 13:35:00 -0400

    initial commit

---
+++ TwitterAPI.py
@@ -0,0 +1,103 @@
+import tweepy
+from textblob import TextBlob
+import re
+
+
+def get_tweets(search_query, number_tweets=50):
+    """
+    Gets a list of tweets
+    :param search_query: search query used to find tweets (String)
+    :param number_tweets: number of tweets to get (Integer)
+    :return: List of tweets
+    """
+    consumer_key = 'RLBnivPChhdTjB9s2lL1M88Po'
+    consumer_secret = 'UFXipb6TJb9H9vGolaZoxQczRbPfsy5QafwYSlK80ypAJvfmYx'
+    access_token = '850748910930972672-wTQ7Gm0zUhrhipOm49N4gpWW6CXwZer'
+    access_token_secret = 'Tnj3vU8kc51rti8kfZRRvphfs7vDD6dfbDcYLYpnEv8uP'
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    tweets = api.search(q=search_query, count=number_tweets)
+    text = []
+    for tweet in tweets:
+        new_text = scrub_tweet(tweet.text)
+        if len(new_text) > 0:
+            text.append(new_text)
+    return text
+
+
+def scrub_tweet(tweet):
+    """
+    Scubs a list of tweets
+    :param tweet: tweet (String)
+    :return: scrubed_tweets: Scrubbed list of tweets (List of Strings)
+    """
+    tweet = tweet.lower()
+    tweet = re.sub(r'https://.*', '', tweet)
+
+    tweet = re.sub(r'rt.*?:', '', tweet)
+    tweet = re.sub(r'\(.*?.\)', '', tweet)
+    tweet = re.sub(r'@.*? ', '', tweet)
+    tweet = re.sub(r'@.*?', '', tweet)
+    tweet = re.sub(r'#.*? ', '', tweet)
+    tweet = tweet.replace("&gt", ' ')
+    tweet = tweet.replace("&lt", ' ')
+    tweet = tweet.replace("&amp", ' ')
+
+    tweet = tweet.replace(u'\u2014', '-')
+    tweet = tweet.replace(u'\u2013', '-')
+    exclude = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+    exclude += ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    exclude.append(u'\u2018')  # '
+    exclude.append(u'\u2019')  # '
+    exclude.append(u'\u201c')  # "
+    exclude.append(u'\u201d')  # "
+    exclude.append(u'\u2022')  # bullet point
+    exclude.append(u'\u2026')  # ...
+
+    for c in exclude:
+        tweet = tweet.replace(c, ' ')
+        tweet = tweet.strip()
+    tweet.replace("\n", " ")
+    tweet = tweet.replace('-', ' ')
+    scrubed_tweet = ' '.join(tweet.split())
+    return scrubed_tweet
+
+
+def get_tweet_sentiment(tweet):
+    '''
+    Utility function to classify sentiment of passed tweet
+    using textblob's sentiment method
+    '''
+    # create TextBlob object of passed tweet text
+    analysis = TextBlob(tweet)
+    # set sentiment
+    if analysis.sentiment.polarity > 0:
+        return 1 #positive
+    elif analysis.sentiment.polarity == 0:
+        return 0 #neutral
+    else:
+        return -1 #negative
+
+
+def get_sentiments(search_query, number_tweets):
+    tweets = get_tweets(search_query, number_tweets)
+    positive = 0
+    neutral = 0
+    negative = 0
+
+    for tweet in tweets:
+        sentiment = get_tweet_sentiment(tweet)
+        if sentiment > 0:
+            positive += 1
+        elif sentiment == 0:
+            neutral += 1
+        else:
+            negative += 1
+
+    empty_tweets = number_tweets - (positive + neutral + negative)
+
+    return [positive, neutral, negative, empty_tweets]
+
---
+++ app/views.py
@@ -0,0 +1,16 @@
+from app import app
+from flask import render_template, request
+from TwitterAPI import get_sentiments
+
+
+@app.route('/')
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if 'reset' in request.form:
+            return render_template("index.html")
+        search_query = request.form['search_query']
+        quantity = request.form['quantity']
+        results = get_sentiments(search_query, int(quantity))
+        return render_template("results.html", results=results)
+    return render_template("index.html")
